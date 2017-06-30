6b64762d8a69f3a35ab163b337fee8f139a2c407
joschne <jonas.schneider@balcab.ch>
2017-06-30 18:13:55 +0200

    Revert "added basic postgres datasource"

--- server/scripts/create-tables.js
+++
@@ -1,8 +0,0 @@
-var server = require('./server');
-var ds = server.dataSources.postgres1;
-var lbTables = ['User', 'AccessToken', 'ACL', 'RoleMapping', 'Role', 'item'];
-ds.automigrate(lbTables, function(er) {
-  if (er) throw er;
-  console.log('Loopback tables [' - lbTables - '] created in ', ds.adapter.name);
-  ds.disconnect();
-});
