b0beef4989e5edaa69bf285453638b2c6d8fc092
joschne <jonas.schneider@balcab.ch>
2018-02-21 11:47:14 +0100

    Drop temporary column in dfh.class
    Before, the table data_for_history.class had a temporary column called data_for_history_id.
    
    This migration drops the column.

---
+++ server/migrations/20180220171107-dfh-t-class-drop-a-col.js
@@ -0,0 +1,48 @@
+'use strict';
+
+var dbm;
+var type;
+var seed;
+
+/**
+  * We receive the dbmigrate dependency from dbmigrate initially.
+  * This enables us to not have to rely on NODE_PATH.
+  */
+exports.setup = function(options, seedLink) {
+  dbm = options.dbmigrate;
+  type = dbm.dataType;
+  seed = seedLink;
+};
+
+exports.up = function(db, callback) {
+
+
+  const sql = `
+  ALTER TABLE data_for_history.class
+  DROP CONSTRAINT class_data_for_history_id_key;
+
+  ALTER TABLE data_for_history.class
+  DROP COLUMN data_for_history_id;
+  `
+
+  db.runSql(sql, callback)
+
+};
+
+exports.down = function(db, callback) {
+
+  const sql = `
+  ALTER TABLE data_for_history.class
+  ADD COLUMN data_for_history_id VARCHAR(7) NOT NULL;
+
+  ALTER TABLE data_for_history.class
+  ADD CONSTRAINT class_data_for_history_id_key
+  UNIQUE (data_for_history_id);
+  `
+
+  db.runSql(sql, callback)
+
+};
+exports._meta = {
+  "version": 1
+};
