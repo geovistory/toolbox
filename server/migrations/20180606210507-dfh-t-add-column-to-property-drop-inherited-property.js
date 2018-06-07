bd8af6d86273bb142c50dfd3378013925357d112
fberetta <fb_listes@bluewin.ch>
2018-06-07 10:18:02 +0200

    New migration: Added inherited property column to data_for_history.property table. Dropped data_for_history.inherited_property_table

---
+++ server/migrations/20180606210507-dfh-t-add-column-to-property-drop-inherited-property.js
@@ -0,0 +1,94 @@
+'use strict';
+
+var dbm;
+var type;
+var seed;
+
+/**
+  * We receive the dbmigrate dependency from dbmigrate initially.
+  * This enables us to not have to rely on NODE_PATH.
+  */
+exports.setup = function(options, seedLink) {
+  dbm = options.dbmigrate;
+  type = dbm.dataType;
+  seed = seedLink;
+};
+
+exports.up = function(db, callback) {
+
+
+  const sql = `
+  ALTER TABLE data_for_history.property
+    ADD COLUMN dfh_fk_property_of_origin INTEGER;
+  ALTER TABLE data_for_history.property_vt
+    ADD COLUMN dfh_fk_property_of_origin INTEGER;
+  -- the data_for_history.inherited_property isn't any more in use, even not in OntoME
+  DROP TABLE IF EXISTS data_for_history.inherited_property CASCADE;
+  DROP TABLE IF EXISTS data_for_history.inherited_property_vt CASCADE;
+  `
+
+  db.runSql(sql, callback)
+
+};
+
+exports.down = function(db, callback) {
+
+  const sql = `
+
+    ALTER TABLE data_for_history.property
+      DROP COLUMN dfh_fk_property_of_origin;
+    ALTER TABLE data_for_history.property_vt
+        DROP COLUMN dfh_fk_property_of_origin;
+
+    CREATE TABLE data_for_history.inherited_property (
+      dfh_pk_inherited_property         integer,
+      dfh_is_originated_from_property   integer,
+      dfh_inherited_from_class              integer,
+      dfh_identifier_in_namespace       text,
+      dfh_has_domain                    integer,
+      dfh_has_range                     integer,
+      dfh_creation_time                 timestamp,
+      dfh_modification_time             timestamp,
+      dfh_domain_instances_min_quantifier  smallint,
+      dfh_domain_instances_max_quantifier  smallint,
+      dfh_range_instances_min_quantifier   smallint,
+      dfh_range_instances_max_quantifier   smallint,
+      dfh_standard_label                varchar(500)
+    )
+    INHERITS (data_for_history.entity);
+
+    CREATE TRIGGER creation_tmsp
+    BEFORE INSERT
+    ON data_for_history.inherited_property
+    FOR EACH ROW
+    EXECUTE PROCEDURE commons.tmsp_creation();
+
+    CREATE TRIGGER last_modification_tmsp
+    BEFORE INSERT OR UPDATE
+    ON data_for_history.inherited_property
+    FOR EACH ROW
+    EXECUTE PROCEDURE commons.tmsp_last_modification();
+
+    CREATE TRIGGER insert_schema_table_name BEFORE INSERT
+    ON data_for_history.inherited_property FOR EACH ROW
+    EXECUTE PROCEDURE commons.insert_schema_table_name();
+
+
+    -- versioning
+
+    CREATE TABLE data_for_history.inherited_property_vt (LIKE data_for_history.inherited_property);
+
+    CREATE TRIGGER versioning_trigger
+    BEFORE INSERT OR UPDATE OR DELETE ON data_for_history.inherited_property
+    FOR EACH ROW EXECUTE PROCEDURE versioning(
+      'sys_period', 'data_for_history.inherited_property_vt', true
+    );
+
+  `
+
+  db.runSql(sql, callback)
+
+};
+exports._meta = {
+  "version": 1
+};
