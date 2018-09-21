4facb1d8c4c8a5be93abbf5be891adef2b8ad1d2
joschne <jonas.schneider@balcab.ch>
2018-09-21 09:06:26 +0200

    finalized and tested migration for dfh.proj_rel

--- server/migrations/20180921064028-dfh-t-proj-rel.js
+++ server/migrations/20180921064028-dfh-t-proj-rel.js
@@ -14,12 +14,54 @@ exports.setup = function(options, seedLink) {
   seed = seedLink;
 };
 
-exports.up = function(db) {
-  return null;
+exports.up = function(db, callback) {
+  const sql = `
+  CREATE TABLE data_for_history.proj_rel
+  (
+    pk_entity integer primary key,
+    fk_project integer references commons.project (pk_project),
+    fk_entity integer references data_for_history.entity (pk_entity)
+  )
+  INHERITS (data_for_history.entity);
+
+  CREATE TRIGGER creation_tmsp
+      BEFORE INSERT
+      ON data_for_history.proj_rel
+      FOR EACH ROW
+      EXECUTE PROCEDURE commons.tmsp_creation();
+
+  CREATE TRIGGER last_modification_tmsp
+  BEFORE INSERT OR UPDATE
+      ON data_for_history.proj_rel
+      FOR EACH ROW
+      EXECUTE PROCEDURE commons.tmsp_last_modification();
+
+  CREATE TRIGGER insert_schema_table_name BEFORE INSERT
+    ON data_for_history.proj_rel FOR EACH ROW
+    EXECUTE PROCEDURE commons.insert_schema_table_name();
+
+
+  -- versioning
+
+  CREATE TABLE data_for_history.proj_rel_vt (LIKE data_for_history.proj_rel);
+
+  CREATE TRIGGER versioning_trigger
+  BEFORE INSERT OR UPDATE OR DELETE ON data_for_history.proj_rel
+  FOR EACH ROW EXECUTE PROCEDURE versioning(
+  'sys_period', 'data_for_history.proj_rel_vt', true
+  );
+
+  `
+  db.runSql(sql, callback)
+
 };
 
-exports.down = function(db) {
-  return null;
+exports.down = function(db, callback) {
+  const sql = `
+  DROP TABLE data_for_history.proj_rel CASCADE;
+  DROP TABLE data_for_history.proj_rel_vt;
+  `
+  db.runSql(sql, callback)
 };
 
 exports._meta = {
