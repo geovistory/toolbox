839ca55d9a183995a2fa42697b77abeb782b5a7e
joschne <jonas.schneider@balcab.ch>
2018-12-17 19:34:05 +0100

    fixes

--- deployment/setup-review-db.sh
+++ deployment/setup-review-db.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # echo '============================= SETUP REVIEW DB ============================= ';
 
-# DB_SOURCE='postgres://qephbxmaqlbbsi:f443e911ee88f21491add8d4ae9ddca4c71e5f81d67e47685bae2d152bf8c6a9@ec2-54-228-251-254.eu-west-1.compute.amazonaws.com:5432/de8ucdfdgv0slb';
-# DB_TARGET=$DATABASE_URL;
+DB_SOURCE='postgres://qephbxmaqlbbsi:f443e911ee88f21491add8d4ae9ddca4c71e5f81d67e47685bae2d152bf8c6a9@ec2-54-228-251-254.eu-west-1.compute.amazonaws.com:5432/de8ucdfdgv0slb';
+DB_TARGET=$DATABASE_URL;
 
 # echo 'copy data'
 # echo 'from: '$DB_SOURCE;
@@ -18,8 +18,7 @@
 # psql $DB_TARGET -f dropSchemas.sql
 
 echo '================= MIGRATE REVIEW DB UP TO STATE OF STAGING ================';
-
-latest_migration=$(node migrate-to-staging-schema.js $DB_SOURCE);
+latest_migration=`psql $DB_SOURCE -t -c "SELECT name from public.migrations ORDER BY id DESC LIMIT 1"`
 echo 'latest migration of staging: '$latest_migration;
 ../node_modules/db-migrate/bin/db-migrate --config ../server/migrate-db-config.json --migrations-dir ../server/migrations up latest_migration;
 
