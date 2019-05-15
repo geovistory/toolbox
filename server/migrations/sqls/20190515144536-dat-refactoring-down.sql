fc6e228c51199cb56d62db982eeee7012106f976
joschne <jonas.schneider@balcab.ch>
2019-05-15 16:56:29 +0200

    refactor data schema

---
+++ server/migrations/sqls/20190515144536-dat-refactoring-down.sql
@@ -0,0 +1,2 @@
+-- 1
+SELECT commons.rename_versioned_table('data', 'factoid_mapping', 'factoid_class_digital_rel')
---
+++ server/migrations/sqls/20190515144536-dat-refactoring-up.sql
@@ -0,0 +1,2 @@
+-- 1
+SELECT commons.rename_versioned_table('data', 'factoid_class_digital_rel', 'factoid_mapping')
