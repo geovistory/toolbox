e582e357198480dcd0364eb48a57337cd97ab33f
joschne <jonas.schneider@balcab.ch>
2019-06-06 15:03:30 +0200

    updated system type table

--- server/migrations/sqls/20190606120608-sys-t-system-type-update-down.sql
+++ server/migrations/sqls/20190606120608-sys-t-system-type-update-down.sql
@@ -1,3 +1,10 @@
+-- 9
+ALTER TABLE projects.text_property_vt DROP COLUMN fk_language;
+
+-- 2
+ALTER TABLE system.system_type_vt DROP COLUMN entity_version;
+ALTER TABLE system.system_type_vt ADD COLUMN pk_system_type INT;
+
 -- 1
 ALTER TABLE system.system_type DROP COLUMN definition;
 ALTER TABLE system.system_type DROP COLUMN st_column_name;
--- server/migrations/sqls/20190606120608-sys-t-system-type-update-up.sql
+++ server/migrations/sqls/20190606120608-sys-t-system-type-update-up.sql
@@ -5,3 +5,10 @@ ALTER TABLE system.system_type ADD COLUMN "group" text;
 ALTER TABLE system.system_type_vt ADD COLUMN definition text;
 ALTER TABLE system.system_type_vt ADD COLUMN st_column_name text;
 ALTER TABLE system.system_type_vt ADD COLUMN "group" text;
+
+-- 2
+ALTER TABLE system.system_type_vt ADD COLUMN entity_version INT;
+ALTER TABLE system.system_type_vt DROP COLUMN pk_system_type; -- no way back
+
+-- 9
+ALTER TABLE projects.text_property_vt ADD COLUMN fk_language INT;
