a6cb6c948b1a4089ec325769db1b949d16549b1e
joschne <jonas.schneider@balcab.ch>
2019-06-11 11:25:31 +0200

    fixed missing redirect url and password missmatch

--- client/src/app/core/validation/validation.directives.ts
+++ client/src/app/core/validation/validation.directives.ts
@@ -1,5 +1,5 @@
 import { ValidationService } from './validation.service';
-import { Directive, Input, NgModule } from '@angular/core';
+import { Directive, Input, NgModule, forwardRef, Attribute } from '@angular/core';
 import { NG_VALIDATORS, Validator, FormGroup, AbstractControl } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 
@@ -15,19 +15,60 @@ export class NoInvalidChildrenDirective implements Validator {
   }
 }
 
+@Directive({
+  selector: '[validateEqual]',
+  providers: [
+    {
+      provide: NG_VALIDATORS,
+      useExisting: forwardRef(() => EqualValidator), multi: true
+    }
+  ]
+})
+export class EqualValidator implements Validator {
+  
+  @Input('validateEqual') public validateEqual: string
+  @Input('reverse') public reverse: string
+
+  constructor() { }
+
+  private get isReverse() {
+    if (!this.reverse) return false;
+    return this.reverse === 'true' ? true : false;
+  }
+  validate(c: AbstractControl): { [key: string]: any } {
+    // self value
+    let v = c.value;
+    // control value
+    let e = c.root.get(this.validateEqual);
+
+    // value not equal
+    if (e && v !== e.value && !this.isReverse) return {
+      validateEqual: false
+    }
+    // value equal and reverse
+    if (e && v === e.value && this.isReverse) {
+      delete e.errors['validateEqual'];
+      if (!Object.keys(e.errors).length) e.setErrors(null);
+    }
+    // value not equal and reverse
+    if (e && v !== e.value && this.isReverse) {
+      e.setErrors({ validateEqual: false })
+    }
+    return null;
+  }
+}
+
+
 const directives = [
-  NoInvalidChildrenDirective
+  NoInvalidChildrenDirective,
+  EqualValidator
 ]
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [
-    NoInvalidChildrenDirective
-  ],
-  exports: [
-    NoInvalidChildrenDirective
-  ]
+  declarations: directives,
+  exports: directives
 })
 export class ValidationDirectivesModule { }
