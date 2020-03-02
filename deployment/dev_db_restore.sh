9e90c20a6430a7ede345ee5d1669402c8535fe44
joschne <jonas.schneider@balcab.ch>
2020-03-02 10:56:15 +0100

    helper script for developers

---
+++ deployment/dev_db_restore.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+#
+# Helper script for developers:
+# Restore reviewdb.tar on a local database
+#
+
+read -p 'Username [postgres]: ' user
+user=${user:-postgres}
+echo $user
+echo
+
+read -p 'Password [GuruAtMarly]: ' password
+password=${password:-GuruAtMarly}
+echo $password
+echo
+
+read -p 'Host [localhost]: ' host
+host=${host:-localhost}
+echo $host
+echo
+
+read -p 'Port [5432]: ' port
+port=${port:-5432}
+echo $port
+echo
+
+read -p 'Database [dev1]: ' database
+database=${database:-dev1}
+echo $database
+echo
+
+read -p 'File [reviewdb.tar]: ' filepath
+filepath=${filepath:-reviewdb.tar}
+echo $filepath
+echo
+
+echo Your command: pg_restore --no-owner -d postgres://$user:$password@$host:$port/$database $filepath --verbose
+
+while true; do
+  read -p "Do you want to execute the command [y/n]?" yn
+  case $yn in
+  [Yy]*)
+    ^pg_restore --no-owner -d postgres://$user:$password@$host:$port/$database $filepath --verbose
+    break
+    ;;
+  [Nn]*) exit ;;
+  *) echo "Please answer yes or no." ;;
+  esac
+done
