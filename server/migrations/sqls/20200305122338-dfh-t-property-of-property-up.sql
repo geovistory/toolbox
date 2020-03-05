02e1d70b3ecc87dc1ef861142530728ae7187fb2
joschne <jonas.schneider@balcab.ch>
2020-03-05 15:01:38 +0100

    migrations

---
+++ server/migrations/sqls/20200305122338-dfh-t-property-of-property-up.sql
@@ -0,0 +1,6 @@
+Alter Table data_for_history.property_of_property
+    Drop Column dfh_fk_property_of_property_of_origin;
+
+Alter Table data_for_history.property_of_property_vt
+    Drop Column dfh_fk_property_of_property_of_origin;
+
