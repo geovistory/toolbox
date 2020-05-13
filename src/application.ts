bd0af0f1dbe68dc47505f8b6603e327191f44aed
joschne <jonas.schneider@balcab.ch>
2020-05-13 20:07:21 +0200

    Mounting LB3 app on LB4 app

---
+++ src/application.ts
@@ -0,0 +1,46 @@
+import {BootMixin} from '@loopback/boot';
+import {Lb3AppBooterComponent} from '@loopback/booter-lb3app';
+import {ApplicationConfig} from '@loopback/core';
+import {RepositoryMixin} from '@loopback/repository';
+import {RestApplication} from '@loopback/rest';
+import {RestExplorerBindings, RestExplorerComponent} from '@loopback/rest-explorer';
+import {ServiceMixin} from '@loopback/service-proxy';
+import path from 'path';
+import {MySequence} from './sequence';
+
+export class GeovistoryApplication extends BootMixin(
+  ServiceMixin(RepositoryMixin(RestApplication)),
+) {
+  constructor(options: ApplicationConfig = {}) {
+    super(options);
+
+    // Set up the custom sequence
+    this.sequence(MySequence);
+
+    // Set up default home page
+    this.static('/', path.join(__dirname, '../public'));
+
+    // Customize @loopback/rest-explorer configuration here
+    this.configure(RestExplorerBindings.COMPONENT).to({
+      path: '/explorer',
+    });
+    this.component(RestExplorerComponent);
+
+    // Register Loopback 3 app as a component
+    // TODO:
+    // - Migrate Lb3 to Lb4 and remove the following line
+    // - run: npm uninstall --save @loopback/booter-lb3app
+    this.component(Lb3AppBooterComponent);
+
+    this.projectRoot = __dirname;
+    // Customize @loopback/boot Booter Conventions here
+    this.bootOptions = {
+      controllers: {
+        // Customize ControllerBooter Conventions here
+        dirs: ['controllers'],
+        extensions: ['.controller.js'],
+        nested: true,
+      },
+    };
+  }
+}
