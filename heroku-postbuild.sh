cf08afb3c9938175ccd6cbab2dc4b3ae7603f794
joschne <jonas.schneider@balcab.ch>
2020-07-15 15:37:03 +0200

    deploy-fix-2

--- heroku-postbuild.sh
+++ heroku-postbuild.sh
@@ -3,9 +3,11 @@
 echo '================ Start of heroku-postbuild.sh =============================='
 
 echo '================= Compile Server =============================='
-
 cd ./server 
+echo './server: npm run build'
 npm run build
+echo './server: npm prune --production'
+npm prune --production
 cd ..
 
 echo 'Current environment: DB_ENV="'$DB_ENV'"'
