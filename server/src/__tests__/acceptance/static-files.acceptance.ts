3553d72cffc38d412109a261634d8e9b141984ff
kleiolab <kleiolab@localhost.localdomain>
2020-07-30 13:53:26 +0200

    client built

--- server/src/__tests__/acceptance/static-files.acceptance.ts
+++ server/src/__tests__/acceptance/static-files.acceptance.ts
@@ -1,18 +1,22 @@
-import {Client} from '@loopback/testlab';
-import {GeovistoryServer} from '../../server';
-import {setupApplication} from './_test-helper';
+import { Client } from '@loopback/testlab';
+import { GeovistoryServer } from '../../server';
+import { setupApplication } from './_test-helper';
+import { cleanDb } from '../helpers/cleaning/clean-db.helper';
+
 
 describe('Static Files', () => {
   let server: GeovistoryServer;
   let client: Client;
 
   before('setupApplication', async () => {
-    ({server, client} = await setupApplication());
+    ({ server, client } = await setupApplication());
   });
 
   after(async () => {
     await server.stop();
   });
+
+  beforeEach(async () => cleanDb());
 
   it('exposes angular index.html', async () => {
     await client
