2689ca2be155a081290781d24a795b183d52942a
kleiolab <kleiolab@localhost.localdomain>
2020-09-15 10:30:05 +0200

    importer review db - testdb

--- server/src/datasources/testdb.datasource.ts
+++ server/src/datasources/testdb.datasource.ts
@@ -1,5 +1,5 @@
-import {inject, lifeCycleObserver, LifeCycleObserver} from '@loopback/core';
-import {juggler} from '@loopback/repository';
+import { LifeCycleObserver } from '@loopback/core';
+import { juggler } from '@loopback/repository';
 
 // const config = {
 //   url: process.env.TEST_DATABASE_URL + '?ssl=true',
@@ -14,7 +14,7 @@ import {juggler} from '@loopback/repository';
 // application is stopped. This allows the application to be shut down
 // gracefully. The `stop()` method is inherited from `juggler.DataSource`.
 // Learn more at https://loopback.io/doc/en/lb4/Life-cycle.html
-@lifeCycleObserver('datasource')
+// @lifeCycleObserver('datasource')
 export class TestdbDataSource extends juggler.DataSource
   implements LifeCycleObserver {
   static dataSourceName = 'testdb';
