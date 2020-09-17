3bfa40349f864ba6955c633c2629072e89aca023
kleiolab <kleiolab@localhost.localdomain>
2020-09-17 11:04:50 +0200

    bug correction importer 3

--- client/src/app/modules/data/components/importer/importer.component.ts
+++ client/src/app/modules/data/components/importer/importer.component.ts
@@ -388,7 +388,7 @@ export class ImporterComponent implements OnInit, OnDestroy {
               this.importTableSocket.on('state_' + this.fkDigital, (state: { id: number, advancement: number, infos: string }) => {
                 if (this.fkDigital == state.id) {
                   this.socketMessage$.next(state.infos);
-                  if (state.advancement == 100) {
+                  if (state.advancement == 100 && state.infos != 'inexisting') {
                     if (state.infos == '') state.infos = 'Your table has correctly been imported';
                     this.mode = 'drag-and-drop';
                     this.loaded('Table Uploaded', state.infos);
