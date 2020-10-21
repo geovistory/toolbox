5036d657e43475afe47c5e858cff31a36cd4912a
joschne <jonas.schneider@balcab.ch>
2020-10-21 10:05:30 +0200

    added script to launch dev warehouse without bucketeer

---
+++ server/warehouse.cleanAndDev.js
@@ -0,0 +1,11 @@
+const warhouse = require('./dist/warehouse');
+
+warhouse
+  .cleanAndStartDev()
+  .catch(err => {
+    console.log(`***************************************`);
+    console.error('Warehouse was up and running, when this error occured:');
+    console.error(err);
+    console.log(`***************************************`);
+    process.exit(1);
+  });
