11ad16e82c5043a4ecceb300fe679d3ee7f071f1
joschne <jonas.schneider@balcab.ch>
2020-12-07 16:34:12 +0100

    Warehouse > REntityClassLabel in pure postgres

--- server/src/warehouse/aggregator-ds/entity-class-label/r-entity-class-label/REntityClassLabelService.ts
+++ server/src/warehouse/aggregator-ds/entity-class-label/r-entity-class-label/REntityClassLabelService.ts
@@ -7,6 +7,8 @@ import {Warehouse} from '../../../Warehouse';
 import {RClassLabelService, RClassLabelValue} from '../../class-label/r-class-label/RClassLabelService';
 import {REntityClassLabelAggregator} from './REntityClassLabelAggregator';
 import {REntityClassLabelProviders} from './REntityClassLabelPoviders';
+import {PoolClient} from 'pg';
+import {AggregatorSqlBuilder, CustomValSql} from '../../../base/classes/AggregatorSqlBuilder';
 
 
 export interface REntityClassLabelVal {entityClassLabel?: string}
@@ -17,7 +19,7 @@ export class REntityClassLabelService extends AggregatedDataService2<REntityId, 
     providers = REntityClassLabelProviders;
     depREntity: DependencyIndex<REntityId, REntityClassLabelVal, REntityId, REntity>
     depRClassLabel: DependencyIndex<REntityId, REntityClassLabelVal, RClassId, RClassLabelValue>
-
+    batchSize = 100000;
     constructor(
         @Inject(forwardRef(() => Warehouse)) wh: Warehouse,
         @Inject(forwardRef(() => REntityService)) rEntity: REntityService,
@@ -37,11 +39,54 @@ export class REntityClassLabelService extends AggregatedDataService2<REntityId, 
     onUpsertSql(tableAlias: string) {
         return `
         UPDATE war.entity_preview
-        SET class_label = ${tableAlias}.val->>'entityClassLabel'
+        SET class_label = val->>'entityClassLabel'
         FROM ${tableAlias}
-        WHERE pk_entity = ${tableAlias}."pkEntity"
+        WHERE pk_entity = "pkEntity"
         AND project = 0
-        AND class_label IS DISTINCT FROM ${tableAlias}.val->>'entityClassLabel'`
+        AND class_label IS DISTINCT FROM val->>'entityClassLabel'`
+    }
+
+
+    async aggregateBatch(client: PoolClient, limit: number, offset: number, currentTimestamp: string): Promise<number> {
+        const builder = new AggregatorSqlBuilder(this, client, currentTimestamp, limit, offset)
+        /**
+        * join entity
+        */
+        const pentity = await builder.joinProviderThroughDepIdx({
+            leftTable: builder.batchTmpTable.tableDef,
+            joinWithDepIdx: this.depREntity,
+            joinOnKeys: {
+                pkEntity: {leftCol: 'pkEntity'},
+            },
+            conditionTrueIf: {
+                providerKey: {pkEntity: 'IS NOT NULL'}
+            },
+            createCustomObject: (() => `jsonb_build_object('fkClass', (t2.val->>'fkClass')::int)`) as CustomValSql<{fkClass: number}>,
+        })
+
+        /**
+        * join pClassLabel, create value and upsert it
+        */
+        await builder.joinProviderThroughDepIdx({
+            leftTable: pentity.aggregation.tableDef,
+            joinWithDepIdx: this.depRClassLabel,
+            joinOnKeys: {
+                pkClass: {leftCustom: {name: 'fkClass', type: 'int'}},
+            },
+            conditionTrueIf: {
+                providerKey: {pkClass: 'IS NOT NULL'}
+            },
+            createAggregationVal: {
+                sql: () => `jsonb_build_object('entityClassLabel', t2.val->>'label')`,
+                upsert: true
+            }
+
+        })
+
+        builder.registerUpsertHook()
+        // await builder.printQueries()
+        const count = await builder.executeQueries()
+        return count
     }
 }
 
