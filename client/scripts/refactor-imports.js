ed6411f8677d14f1f7bb03df2001887f8dd470fb
joschne <jonas.schneider@balcab.ch>
2021-02-05 13:08:51 +0100

    refactored import {U} from 'app/core'

---
+++ client/scripts/refactor-imports.js
@@ -0,0 +1,41 @@
+const { readdirSync } = require("fs");
+const { spawn } = require("child_process");
+const path = require("path");
+
+const getDirectories = (source) =>
+  readdirSync(source, { withFileTypes: true })
+    .filter((dirent) => dirent.isDirectory())
+    .map((dirent) => path.join(source, dirent.name));
+
+async function refactor(path) {
+  console.log(`Refactor files in: ${path}`);
+
+  return new Promise((res, rej) => {
+    // DOCUMENTATION: https://github.com/luchsamapparat/refactor-imports
+    const child = spawn("refactor-imports", [
+      "-p",
+      path,
+      "-s",
+      "app/core",
+      "-t",
+      "app/core/util/util",
+      "-e",
+      "U",
+      // "-d",
+    ]);
+
+    child.stdout.pipe(process.stdout);
+    child.stderr.pipe(process.stdout);
+    child.on("close", (code) => {
+      console.log(`child process exited with code ${code}`);
+      res();
+    });
+  });
+}
+async function refactorAll() {
+  const directories = getDirectories("../src/app");
+  for (const path of directories) {
+    await refactor(path);
+  }
+}
+refactorAll().catch((e) => console.log(e));
