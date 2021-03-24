a1a117b5f87b90a1c44a61bdc73658883da33ced
gaetanmuck <gaetan.muck@gmail.com>
2021-03-24 14:54:52 +0100

    test

--- client/projects/app-toolbox/src/app/modules/projects/containers/project-edit/project-edit.component.ts
+++ client/projects/app-toolbox/src/app/modules/projects/containers/project-edit/project-edit.component.ts
@@ -203,6 +203,7 @@ export class ProjectEditComponent implements OnDestroy, AfterViewInit {
     // DEV: For development of a component in a specific Tab uncomment and modify the following
     // this.p.addTableTab(2001)
     // this.p.addTableTab(100011)
+    //test
   }
 
   trackByFn(index, item) {
