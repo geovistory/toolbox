d0153c1d2fd1d3cfb202a62c8f9e970ac01b0b69
Gaétan Muck <gaetan.muck@gmail.com>
2021-04-29 18:27:01 +0200

    Update update-staging-db.sh

--- deployment/update-staging-db.sh
+++ deployment/update-staging-db.sh
@@ -34,16 +34,16 @@ if [ $DB_ENV='temp' ]; then
     heroku ps:scale worker=0 -a geovistory-temp
 
     # delete the warehouse schema name
-    name=$(heroku pg:psql -a geovistory-staging -c "SELECT schema_name FROM information_schema.schemata WHERE schema_name LIKE 'war_cache_%'" WH_DATABASE_URL)
+    name=$(heroku pg:psql -a geovistory-temp -c "SELECT schema_name FROM information_schema.schemata WHERE schema_name LIKE 'war_cache_%'" WH_DATABASE_URL)
     name=${name/schema_name/''} # remove the string 'schema_name'
     name=${name//-/''} # remove the dashs 
     name=${name/(1 ligne)/''} # remove the string '(1 ligne)', if FR env
-    name=${name/(1 line)/''} # remove the string '(1 line)', if EN env
+    name=${name/(1 row)/''} # remove the string '(1 row)', if EN env
     name=`echo $name | xargs`
-    heroku pg:psql -a geovistory-staging -c "DROP SCHEMA IF EXISTS ${name} CASCADE;" WH_DATABASE_URL
+    heroku pg:psql -a geovistory-temp -c "DROP SCHEMA IF EXISTS ${name} CASCADE;" WH_DATABASE_URL
 
     # copy dev db to temp db
-    heroku pg:copy geovistory-dev::DATABASE_URL DATABASE_URL -a geovistory-temp --confirm=confirm
+    heroku pg:copy geovistory-dev::DATABASE_URL DATABASE_URL -a geovistory-temp --confirm=geovistory-temp
 
     # turn other workers ON
     heroku ps:scale worker=1 -a geovistory-temp
