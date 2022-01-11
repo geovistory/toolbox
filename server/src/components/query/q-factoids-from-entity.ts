3c309123bc8230d169ea1375c0811340e7364641
gaetanmuck <gaetan.muck@gmail.com>
2022-01-11 13:22:12 +0100

    fetch factoid when default

--- server/src/components/query/q-factoids-from-entity.ts
+++ server/src/components/query/q-factoids-from-entity.ts
@@ -32,6 +32,8 @@ export class QFactoidsFromEntity extends SqlBuilderLb4Models {
      * @param {*} pkEntity primary key of the entity we want factoids from.
      */
     async query(pkProject: string, pkEntity: string, offset: number, size: number): Promise<Array<FactoidEntity>> {
+
+        // fetch all the factoids that has the entity as the matched value in the table
         this.params = [];
         this.sql = `
         WITH tw1 AS(
@@ -83,29 +85,79 @@ export class QFactoidsFromEntity extends SqlBuilderLb4Models {
         `;
 
         this.getBuiltQuery();
-        return this.execute<Array<RetrievedLine>>().then(result => {
-            const factoidsEntities: Array<FactoidEntity> = [];
-            result.forEach(line => {
-                let target = factoidsEntities
-                    .filter(fe => fe.pkClass === line.fkclass && fe.pkRow === parseInt(line.fkrow) && fe.pkFactoidMapping === line.fkfactoidmapping)[0];
-                if (!target) {
-                    target = new FactoidEntity(line.fkdigital, line.fkclass, parseInt(line.fkrow), line.fkfactoidmapping);
-                    factoidsEntities.push(target);
-                }
-
-                const statement = new FactoidStatement(line.fkproperty, line.isoutgoing, line.value, parseInt(line.pkentity), parseInt(line.fkcell), parseInt(line.fkdefault));
-                if (parseInt(line.pkentity) === parseInt(pkEntity)) target.headerStatements.push(statement);
-                else target.bodyStatements.push(statement);
+        const firstQuery = this.execute<Array<RetrievedLine>>()
 
 
-                // if (line.header) {
-                //     target.bodyStatements = target.bodyStatements.filter(stm => stm.pkCell !== statement.pkCell);
-                //     target.headerStatements.push(statement);
-                // } else if (!target.headerStatements.some(stm => stm.pkCell === statement.pkCell)) target.bodyStatements.push(statement);
+        // fetch all the factoids that has the entity as the default value in the factoid property mapping but has no matching in the table
+        this.params = [];
+        this.sql = `
+        select
+            fm.fk_digital as fkDigital,
+            fm.pk_entity as fkFactoidMapping,
+            fm.fk_class as fkClass,
+            fpm2.fk_property as fkProperty,
+            fpm2.is_outgoing as isOutgoing,
+            coalesce(c.numeric_value::text, c.string_value) as value,
+            c.fk_row as fkRow,
+            case when ipr.is_in_project = true then s.fk_object_info else fpm.fk_default end as pkEntity,
+            c.pk_cell as fkCell,
+            fpm.fk_default as fkDefault,
+            ipr.is_in_project
+        from data.factoid_property_mapping fpm
+        left join data.factoid_mapping fm on fm.pk_entity = fpm.fk_factoid_mapping
+        left join data.factoid_property_mapping fpm2 on fpm2.fk_factoid_mapping = fm.pk_entity
+        left join tables.cell c on c.fk_column = fpm2.fk_column
+        left join information.statement s on s.fk_subject_tables_cell = c.pk_cell and s.fk_property = 1334
+        left join projects.info_proj_rel ipr on s.pk_entity = ipr.fk_entity and ipr.is_in_project = true
+        inner join (
+            select * from (
+                select
+                    max(c.fk_row) as fk_row,
+                    c.pk_cell,
+                    bool_or(ipr.is_in_project) as stmt_exists
+                from data.factoid_property_mapping fpm
+                left join tables.cell c on c.fk_column = fpm.fk_column
+                left join information.statement s on s.fk_subject_tables_cell = c.pk_cell and s.fk_property = 1334
+                left join projects.info_proj_rel ipr on ipr.fk_entity = s.pk_entity
+                where
+                    fpm.fk_default = ${this.addParam(pkEntity)}
+                group by c.pk_cell) as t0
+            where t0.stmt_exists = false
+        ) ij on ij.fk_row = c.fk_row
+        where
+            fpm.fk_default = ${this.addParam(pkEntity)}
+        `;
+
+
+        this.getBuiltQuery();
+        const secondQuery = this.execute<Array<RetrievedLine>>()
+
+
+        return Promise.all([firstQuery, secondQuery])
+            .then(([result1, result2]) => result1.concat(result2))
+            .then(result => {
+                const factoidsEntities: Array<FactoidEntity> = [];
+                result.forEach(line => {
+                    let target = factoidsEntities
+                        .filter(fe => fe.pkClass === line.fkclass && fe.pkRow === parseInt(line.fkrow) && fe.pkFactoidMapping === line.fkfactoidmapping)[0];
+                    if (!target) {
+                        target = new FactoidEntity(line.fkdigital, line.fkclass, parseInt(line.fkrow), line.fkfactoidmapping);
+                        factoidsEntities.push(target);
+                    }
+
+                    const statement = new FactoidStatement(line.fkproperty, line.isoutgoing, line.value, parseInt(line.pkentity), parseInt(line.fkcell), parseInt(line.fkdefault));
+                    if (parseInt(line.pkentity) === parseInt(pkEntity)) target.headerStatements.push(statement);
+                    else target.bodyStatements.push(statement);
+
+
+                    // if (line.header) {
+                    //     target.bodyStatements = target.bodyStatements.filter(stm => stm.pkCell !== statement.pkCell);
+                    //     target.headerStatements.push(statement);
+                    // } else if (!target.headerStatements.some(stm => stm.pkCell === statement.pkCell)) target.bodyStatements.push(statement);
+                });
+                return factoidsEntities.sort((a, b) => a.pkClass - b.pkClass)
+                    .slice(offset, offset + size);
             });
-            return factoidsEntities.sort((a, b) => a.pkClass - b.pkClass)
-                .slice(offset, offset + size);
-        });
     }
 
     async getFactoidNumber(pkProject: string, pkEntity: string) {
@@ -130,5 +182,35 @@ export class QFactoidsFromEntity extends SqlBuilderLb4Models {
 
         this.getBuiltQuery()
         return this.execute<Array<{length: number}>>();
+    }
+
+
+    async getFactoidMappingAsDefaultValue(pkEntity: string, offset: number, size: number): Promise<Array<FactoidEntity>> {
+
+
+        this.getBuiltQuery();
+        return this.execute<Array<RetrievedLine>>().then(result => {
+            const factoidsEntities: Array<FactoidEntity> = [];
+            result.forEach(line => {
+                let target = factoidsEntities
+                    .filter(fe => fe.pkClass === line.fkclass && fe.pkRow === parseInt(line.fkrow) && fe.pkFactoidMapping === line.fkfactoidmapping)[0];
+                if (!target) {
+                    target = new FactoidEntity(line.fkdigital, line.fkclass, parseInt(line.fkrow), line.fkfactoidmapping);
+                    factoidsEntities.push(target);
+                }
+
+                const statement = new FactoidStatement(line.fkproperty, line.isoutgoing, line.value, parseInt(line.pkentity), parseInt(line.fkcell), parseInt(line.fkdefault));
+                if (parseInt(line.pkentity) === parseInt(pkEntity)) target.headerStatements.push(statement);
+                else target.bodyStatements.push(statement);
+
+
+                // if (line.header) {
+                //     target.bodyStatements = target.bodyStatements.filter(stm => stm.pkCell !== statement.pkCell);
+                //     target.headerStatements.push(statement);
+                // } else if (!target.headerStatements.some(stm => stm.pkCell === statement.pkCell)) target.bodyStatements.push(statement);
+            });
+            return factoidsEntities.sort((a, b) => a.pkClass - b.pkClass)
+                .slice(offset, offset + size);
+        });
     }
 }
