8880edca0aa9736299f7102f97876077f11960b0
joschne <jonas.schneider@balcab.ch>
2022-04-27 13:23:11 +0200

    Add index to statement table

---
+++ server/db-migrate/migrations/sqls/20220427111358-inf-t-statement-add-idx-down.sql
@@ -0,0 +1,2 @@
+DROP INDEX information.statement_fk_object_tables_cell_idx;
+
---
+++ server/db-migrate/migrations/sqls/20220427111358-inf-t-statement-add-idx-up.sql
@@ -0,0 +1,2 @@
+CREATE INDEX statement_fk_object_tables_cell_idx ON information.statement USING btree (fk_object_tables_cell ASC NULLS LAST);
+
