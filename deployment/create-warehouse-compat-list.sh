e57d9d8764729e8fea8d03cde96904f83478ff7e
joschne <jonas.schneider@balcab.ch>
2022-08-18 17:12:31 +0200

    Update create-warehouse-compat-list.sh

--- deployment/create-warehouse-compat-list.sh
+++ deployment/create-warehouse-compat-list.sh
@@ -30,8 +30,11 @@ rm -f $output
 # loop over old commits as long as no changes to current commit detected
 # first iteration is over the current commit, so that the list starts with current commit
 while [ $i -le 1000 ]; do
+   
   commit=$(git rev-parse --short @~$i)
+  echo "i: $i, currentCommit: $currentCommit, commit: $commit, path: $path"
   git diff --quiet $currentCommit $commit -- $path
+
   if [ $? -eq 0 ]; then
     # No modifications found in directory $path between $currentCommit and $commit.
     # Adding commit to list of commits where warehouse is compatible with current comit
