6244c8049a31cabeb583d6c9ba2b2eff35f37682
joschne <jonas.schneider@balcab.ch>
2023-10-04 12:56:24 +0200

    Merge remote-tracking branch 'origin/main' into ng-update

--- client/projects/app-toolbox/src/app/modules/projects/components/project-card/project-card.component.ts
+++ client/projects/app-toolbox/src/app/modules/projects/components/project-card/project-card.component.ts
@@ -17,9 +17,13 @@ export class ProjectCardComponent implements OnInit {
   PK_PROJECT_OF_DEFAULT_CONFIG_PROJECT = ProConfig.PK_PROJECT_OF_DEFAULT_CONFIG_PROJECT
   PK_PROJECT_OF_SANDBOX_PROJECT = ProConfig.PK_PROJECT_OF_SANDBOX_PROJECT
 
-  projectLanugageLabel$: Observable<String>
-  projectLabel$: Observable<String>
-  projectDescription$: Observable<String>
+  projectLanugageLabel$: Observable<string>
+  projectLabel$: Observable<string>
+  projectDescription$: Observable<string>
+  btn1Label$: Observable<string>
+  btn1Url$: Observable<string>
+  btn2Label$: Observable<string>
+  btn2Url$: Observable<string>
 
   constructor(private p: ActiveAccountPipes) {
   }
@@ -28,6 +32,10 @@ export class ProjectCardComponent implements OnInit {
     this.projectLanugageLabel$ = this.p.getProjectLanguageLabel(this.project);
     this.projectLabel$ = this.p.getProjectLabel(this.project.pk_entity);
     this.projectDescription$ = this.p.getProjectDescription(this.project.pk_entity);
+    this.btn1Label$ = this.p.getProjectBtn1Label(this.project.pk_entity);
+    this.btn1Url$ = this.p.getProjectBtn1Url(this.project.pk_entity);
+    this.btn2Label$ = this.p.getProjectBtn2Label(this.project.pk_entity);
+    this.btn2Url$ = this.p.getProjectBtn2Url(this.project.pk_entity);
   }
 
 }
