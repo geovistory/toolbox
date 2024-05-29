1bf45f29d6d2b5d64514a11c3904634a9c923282
joschne <jonas.schneider@balcab.ch>
2024-05-29 17:40:54 +0200

    add extensions to test db

--- database/postgres/scripts/create_test_db.sh
+++ database/postgres/scripts/create_test_db.sh
@@ -4,3 +4,8 @@ echo "*********************************"
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
     CREATE DATABASE $POSTGRES_DATABASE;
 EOSQL
+
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DATABASE" <<-EOSQL
+    CREATE EXTENSION pldbgapi;
+    CREATE EXTENSION pgtap;
+EOSQL
