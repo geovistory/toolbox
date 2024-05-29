bc669f2e95a40571d242c523002d662010836399
joschne <jonas.schneider@balcab.ch>
2024-05-29 14:19:10 +0200

    fix the path

--- server/scripts/db-migrate-up.js
+++ server/scripts/db-migrate-up.js
@@ -3,6 +3,6 @@ const chooseDB = require('./__chooseGvDb');
 
 async function start() {
   await chooseDB();
-  require('./__execShell').fromFile('../../database/migrations/db-migrate/up.sh');
+  require('./__execShell').fromFile('../db-migrate/up.sh');
 }
 start().catch(e => console.log(e));
