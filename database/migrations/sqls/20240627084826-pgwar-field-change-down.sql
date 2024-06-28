b326500da2bc53836612a7db4efe3deb968dcc55
Djamel Ferhod <djamel.ferhod@cnrs.fr>
2024-06-28 15:46:46 +0200

    added down migration SQL script

--- database/migrations/sqls/20240627084826-pgwar-field-change-down.sql
+++ database/migrations/sqls/20240627084826-pgwar-field-change-down.sql
@@ -1,1 +1,21 @@
-/* Replace with your SQL commands */
+-- Function pgwar.field_change_notify_upsert
+----------------------------------------------
+DROP TRIGGER IF EXISTS  after_insert_field_change ON pgwar.field_change;
+DROP TRIGGER IF EXISTS  after_update_field_change ON pgwar.field_change;
+DROP FUNCTION IF EXISTS  pgwar.field_change_notify_upser;
+
+-- Trigger after_modify_project_statements
+----------------------------------------------
+DROP TRIGGER IF EXISTS  after_modify_project_statements ON pgwar.project_statements;
+
+-- Function upsert_field_change
+----------------------------------------------
+DROP FUNCTION IF EXISTS  pgwar.update_field_change_on_project_statements_modification;
+
+------ Table pgwar.field_change -----------------------------------------------------
+---------------------------------------------------------------------------------------------
+DROP TABLE IF EXISTS  pgwar.field_change;
+
+
+
+
