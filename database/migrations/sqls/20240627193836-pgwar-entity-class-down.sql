44ae2473ee4f7716cd6c1b09e2ce81b7b191e3d6
joschne <jonas.schneider@balcab.ch>
2024-06-28 17:12:57 +0200

    changes according to review

--- database/migrations/sqls/20240627193836-pgwar-entity-class-down.sql
+++ database/migrations/sqls/20240627193836-pgwar-entity-class-down.sql
@@ -1,1 +1,8 @@
-/* Replace with your SQL commands */
+-- Drop the index on the fk_language column in the projects.project table
+DROP INDEX IF EXISTS projects.project_fk_language_idx;
+
+-- Drop the index on the fk_project column in the pgwar.entity_preview table
+DROP INDEX IF EXISTS pgwar.entity_preview_fk_project_idx;
+
+-- Drop the pgwar.update_entity_class function
+DROP FUNCTION IF EXISTS pgwar.update_entity_class();
