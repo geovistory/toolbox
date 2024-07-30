fb95f85c85b8813a3fa31dc78b98d451ea92a5da
Djamel Ferhod <djamel.ferhod@cnrs.fr>
2024-07-30 11:58:29 +0200

    work on integration test for time span still in progress

--- database/test/integration/pgwar-time-span/test-pgwar-time-span.sql
+++ database/test/integration/pgwar-time-span/test-pgwar-time-span.sql
@@ -1,61 +1,195 @@
--- BEGIN;
---
--- SELECT plan(1);
---
--- -- initialize entity previews
--- INSERT INTO pgwar.entity_preview (pk_entity, fk_project, fk_class)
--- VALUES (32, 999, 0),
--- VALUES (32, 999, 0),
--- 	(34, 999, 0);
---
--- -- add full text for two of the entities
--- INSERT INTO pgwar.entity_full_text (pk_entity, fk_project, full_text)
--- VALUES (32, 999, 'full-text-a'),
--- 	(33, 543, 'full-text-b');
---
--- SELECT is(
--- 		full_text,
--- 		'full-text-a',
--- 		'Assert that full-text-a is inserted into entity_preview table after insert'
--- 	)
--- FROM pgwar.entity_preview
--- WHERE pk_entity = 32
--- 	AND fk_project = 999;
---
--- SELECT is(
--- 		full_text,
--- 		'full-text-b',
--- 		'Assert that full-text-b is inserted into entity_preview table after insert'
--- 	)
--- FROM pgwar.entity_preview
--- WHERE pk_entity = 33
--- 	AND fk_project = 543;
---
--- UPDATE pgwar.entity_full_text
--- SET full_text = 'foo'
--- WHERE pk_entity = 33
--- 	AND fk_project = 543;
---
--- SELECT is(
--- 		full_text,
--- 		'foo',
--- 		'Assert that foo is inserted into entity_preview table after update'
--- 	)
--- FROM pgwar.entity_preview
--- WHERE pk_entity = 33
--- 	AND fk_project = 543;
---
--- SELECT is(
--- 		full_text,
--- 		'full-text-a',
--- 		'Assert that entity preview 32-999 is not affected by update'
--- 	)
--- FROM pgwar.entity_preview
--- WHERE pk_entity = 32
--- 	AND fk_project = 999;
---
--- -- Finish the tests and clean up.
--- SELECT *
--- FROM finish();
---
--- ROLLBACK;
+BEGIN;
+
+SELECT plan(1);
+
+-- Drop trigger after_insert_project_statement
+DROP TRIGGER IF EXISTS after_insert_project_statement ON pgwar.project_statements;
+
+-- Drop trigger after_update_project_statement
+DROP TRIGGER IF EXISTS after_update_project_statement ON pgwar.project_statements;
+
+-- Drop trigger after_delete_project_statement
+DROP TRIGGER IF EXISTS after_delete_project_statement ON pgwar.project_statements;
+
+-- Drop trigger after_modify_project_statements
+DROP TRIGGER IF EXISTS after_modify_project_statements ON pgwar.project_statements;
+
+-- initialize entity previews
+INSERT INTO pgwar.entity_preview (pk_entity, fk_project, fk_class)
+VALUES  (32, 999, 0),
+        (33, 999, 0),
+	    (34, 999, 0);
+
+INSERT INTO pgwar.project_statements (pk_entity, fk_project, fk_subject_info, fk_property, object_value)
+VALUES  (132, 999, 32, 72, '{
+                                "timePrimitive": {
+                                  "to": {
+                                    "calJulian": "1890-12-20",
+                                    "julianDay": 2411734,
+                                    "calGregorian": "1891-01-01",
+                                    "julianSecond": 208373817600,
+                                    "calGregorianIso8601": "1891-01-01T00:00:00Z"
+                                  },
+                                  "from": {
+                                    "calJulian": "1889-12-20",
+                                    "julianDay": 2411369,
+                                    "calGregorian": "1890-01-01",
+                                    "julianSecond": 208342281600,
+                                    "calGregorianIso8601": "1890-01-01T00:00:00Z"
+                                  },
+                                  "label": "1890-01-01 (1 year)",
+                                  "fkClass": 335,
+                                  "calendar": "gregorian",
+                                  "duration": "1 year",
+                                  "pkEntity": 835781,
+                                  "julianDay": 2411369
+                                }
+                              }'::jsonb),
+        (133, 999, 33, 72, '{
+                                 "timePrimitive": {
+                                   "to": {
+                                     "calJulian": "1890-12-20",
+                                     "julianDay": 2411734,
+                                     "calGregorian": "1891-01-01",
+                                     "julianSecond": 208373817600,
+                                     "calGregorianIso8601": "1891-01-01T00:00:00Z"
+                                   },
+                                   "from": {
+                                     "calJulian": "1889-12-20",
+                                     "julianDay": 2411369,
+                                     "calGregorian": "1890-01-01",
+                                     "julianSecond": 208342281600,
+                                     "calGregorianIso8601": "1890-01-01T00:00:00Z"
+                                   },
+                                   "label": "1890-01-01 (1 month)",
+                                   "fkClass": 335,
+                                   "calendar": "gregorian",
+                                   "duration": "1 month",
+                                   "pkEntity": 835781,
+                                   "julianDay": 2411369
+                                 }
+                               }'::jsonb),
+	    (134, 999, 34, 72, '{
+                              "timePrimitive": {
+                                "to": {
+                                  "calJulian": "1890-12-20",
+                                  "julianDay": 2411734,
+                                  "calGregorian": "1891-01-01",
+                                  "julianSecond": 208373817600,
+                                  "calGregorianIso8601": "1891-01-01T00:00:00Z"
+                                },
+                                "from": {
+                                  "calJulian": "1889-12-20",
+                                  "julianDay": 2411369,
+                                  "calGregorian": "1890-01-01",
+                                  "julianSecond": 208342281600,
+                                  "calGregorianIso8601": "1890-01-01T00:00:00Z"
+                                },
+                                "label": "1890-01-01 (1 day)",
+                                "fkClass": 335,
+                                "calendar": "gregorian",
+                                "duration": "1 day",
+                                "pkEntity": 835781,
+                                "julianDay": 2411369
+                              }
+                            }'::jsonb);
+
+-- add full text for two of the entities
+INSERT INTO pgwar.statement (pk_entity, fk_subject_info, fk_property, object_value)
+VALUES (132, 32, 72, '{
+                   "timePrimitive": {
+                     "to": {
+                       "calJulian": "1890-12-20",
+                       "julianDay": 2411734,
+                       "calGregorian": "1891-01-01",
+                       "julianSecond": 208373817600,
+                       "calGregorianIso8601": "1891-01-01T00:00:00Z"
+                     },
+                     "from": {
+                       "calJulian": "1889-12-20",
+                       "julianDay": 2411369,
+                       "calGregorian": "1890-01-01",
+                       "julianSecond": 208342281600,
+                       "calGregorianIso8601": "1890-01-01T00:00:00Z"
+                     },
+                     "label": "1890-01-01 (1 year)",
+                     "fkClass": 335,
+                     "calendar": "gregorian",
+                     "duration": "1 year",
+                     "pkEntity": 835781,
+                     "julianDay": 2411369
+                   }
+                 }'::jsonb),
+        (133, 33, 72, '{
+                    "timePrimitive": {
+                      "to": {
+                        "calJulian": "1890-12-20",
+                        "julianDay": 2411734,
+                        "calGregorian": "1891-01-01",
+                        "julianSecond": 208373817600,
+                        "calGregorianIso8601": "1891-01-01T00:00:00Z"
+                      },
+                      "from": {
+                        "calJulian": "1889-12-20",
+                        "julianDay": 2411369,
+                        "calGregorian": "1890-01-01",
+                        "julianSecond": 208342281600,
+                        "calGregorianIso8601": "1890-01-01T00:00:00Z"
+                      },
+                      "label": "1890-01-01 (1 month)",
+                      "fkClass": 335,
+                      "calendar": "gregorian",
+                      "duration": "1 month",
+                      "pkEntity": 835781,
+                      "julianDay": 2411369
+                    }
+                  }'::jsonb),
+	(134, 34, 72, '{
+                 "timePrimitive": {
+                   "to": {
+                     "calJulian": "1890-12-20",
+                     "julianDay": 2411734,
+                     "calGregorian": "1891-01-01",
+                     "julianSecond": 208373817600,
+                     "calGregorianIso8601": "1891-01-01T00:00:00Z"
+                   },
+                   "from": {
+                     "calJulian": "1889-12-20",
+                     "julianDay": 2411369,
+                     "calGregorian": "1890-01-01",
+                     "julianSecond": 208342281600,
+                     "calGregorianIso8601": "1890-01-01T00:00:00Z"
+                   },
+                   "label": "1890-01-01 (1 day)",
+                   "fkClass": 335,
+                   "calendar": "gregorian",
+                   "duration": "1 day",
+                   "pkEntity": 835781,
+                   "julianDay": 2411369
+                 }
+               }'::jsonb);
+
+-- run the update task
+SELECT pgwar.update_time_span();
+
+SELECT is(
+		time_span,
+        '{
+           "p82": {
+             "calendar": "gregorian",
+             "duration": "1 year",
+             "julianDay": 2411369
+           }
+         }'::jsonb,
+		'time span json is correctly constructed'
+	)
+FROM pgwar.entity_preview
+WHERE pk_entity = 32
+	AND fk_project = 999;
+
+
+-- Finish the tests and clean up.
+SELECT *
+FROM finish();
+
+ROLLBACK;
