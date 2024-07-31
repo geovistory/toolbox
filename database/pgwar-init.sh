ff658df63e3610d3ddbf75282d7767009720cf18
dferhod <djamel.ferhod@cnrs>
2024-07-31 15:54:35 +0200

    removed comments

--- database/pgwar-init.sh
+++ database/pgwar-init.sh
@@ -32,82 +32,82 @@ SELECT pg_stat_statements_reset();
 EOF
 
 # Truncate pgwar.entity_preview
-#print_timestamp "Truncating pgwar.entity_preview..."
-#psql $DB_URL <<EOF
-#TRUNCATE pgwar.entity_preview;
-#EOF
+print_timestamp "Truncating pgwar.entity_preview..."
+psql $DB_URL <<EOF
+TRUNCATE pgwar.entity_preview;
+EOF
 
 # Truncate pgwar.project_statements
-#print_timestamp "Truncating pgwar.project_statements..."
-#psql $DB_URL <<EOF
-#TRUNCATE pgwar.project_statements;
-#EOF
+print_timestamp "Truncating pgwar.project_statements..."
+psql $DB_URL <<EOF
+TRUNCATE pgwar.project_statements;
+EOF
 
 # Truncate pgwar.initialization
-#print_timestamp "Truncating pgwar.initialization..."
-#psql $DB_URL <<EOF
-#TRUNCATE pgwar.initialization;
-#EOF
+print_timestamp "Truncating pgwar.initialization..."
+psql $DB_URL <<EOF
+TRUNCATE pgwar.initialization;
+EOF
 
 # Start project statements
-#print_timestamp "Starting project statements..."
-#psql $DB_URL <<EOF
-#    INSERT INTO pgwar.initialization (msg, tmsp) VALUES ('start statements', CLOCK_TIMESTAMP());
-#    SELECT pgwar.update_from_statement(item)
-#    FROM (SELECT i.* FROM information.statement i) AS item;
-#EOF
+print_timestamp "Starting project statements..."
+psql $DB_URL <<EOF
+    INSERT INTO pgwar.initialization (msg, tmsp) VALUES ('start statements', CLOCK_TIMESTAMP());
+    SELECT pgwar.update_from_statement(item)
+    FROM (SELECT i.* FROM information.statement i) AS item;
+EOF
 
 # Start community entities
-#print_timestamp "Starting community entities..."
-#psql $DB_URL <<EOF
-#    INSERT INTO pgwar.initialization (msg, tmsp) VALUES ('start community entities', CLOCK_TIMESTAMP());
-#    INSERT INTO pgwar.entity_preview(pk_entity, fk_project, fk_class, tmsp_fk_class_modification)
-#    SELECT DISTINCT ON (newtab.pk_entity) newtab.pk_entity, 0, newtab.fk_class, CURRENT_TIMESTAMP
-#    FROM information.resource newtab, projects.info_proj_rel ipr
-#    WHERE ipr.fk_entity = newtab.pk_entity AND ipr.is_in_project IS TRUE;
-#EOF
+print_timestamp "Starting community entities..."
+psql $DB_URL <<EOF
+    INSERT INTO pgwar.initialization (msg, tmsp) VALUES ('start community entities', CLOCK_TIMESTAMP());
+    INSERT INTO pgwar.entity_preview(pk_entity, fk_project, fk_class, tmsp_fk_class_modification)
+    SELECT DISTINCT ON (newtab.pk_entity) newtab.pk_entity, 0, newtab.fk_class, CURRENT_TIMESTAMP
+    FROM information.resource newtab, projects.info_proj_rel ipr
+    WHERE ipr.fk_entity = newtab.pk_entity AND ipr.is_in_project IS TRUE;
+EOF
 
 # Start VACUUM ANALYZE
-#print_timestamp "Starting VACUUM ANALYZE..."
-#psql $DB_URL <<EOF
-#    INSERT INTO pgwar.initialization (msg, tmsp) VALUES ('VACUUM ANALYZE', CLOCK_TIMESTAMP());
-#    VACUUM ANALYZE;
-#EOF
+print_timestamp "Starting VACUUM ANALYZE..."
+psql $DB_URL <<EOF
+    INSERT INTO pgwar.initialization (msg, tmsp) VALUES ('VACUUM ANALYZE', CLOCK_TIMESTAMP());
+    VACUUM ANALYZE;
+EOF
 
 # Start project entities 
-#psql $DB_URL <<EOF
-#    INSERT INTO pgwar.initialization (msg, tmsp) VALUES ('start project entities', CLOCK_TIMESTAMP());
-#EOF
-#bash $SCRIPT_DIR/pgwar-init-project-entities/create-tables.sh $CONCURRENT_TASKS $DB_URL;
-#bash $SCRIPT_DIR/pgwar-init-project-entities/loop-over-tables.sh $CONCURRENT_TASKS $DB_URL;
+psql $DB_URL <<EOF
+    INSERT INTO pgwar.initialization (msg, tmsp) VALUES ('start project entities', CLOCK_TIMESTAMP());
+EOF
+bash $SCRIPT_DIR/pgwar-init-project-entities/create-tables.sh $CONCURRENT_TASKS $DB_URL;
+bash $SCRIPT_DIR/pgwar-init-project-entities/loop-over-tables.sh $CONCURRENT_TASKS $DB_URL;
 
 # Start VACUUM ANALYZE
-#print_timestamp "Starting VACUUM ANALYZE..."
-#psql $DB_URL <<EOF
-#    INSERT INTO pgwar.initialization (msg, tmsp) VALUES ('VACUUM ANALYZE', CLOCK_TIMESTAMP());
-#    VACUUM ANALYZE;
-#EOF
+print_timestamp "Starting VACUUM ANALYZE..."
+psql $DB_URL <<EOF
+    INSERT INTO pgwar.initialization (msg, tmsp) VALUES ('VACUUM ANALYZE', CLOCK_TIMESTAMP());
+    VACUUM ANALYZE;
+EOF
 
 # Start community statements
-#print_timestamp "Starting community statements..."
-#psql $DB_URL <<EOF
-#    INSERT INTO pgwar.initialization (msg, tmsp) VALUES ('start community statements', CLOCK_TIMESTAMP());
-#    SELECT pgwar.update_community_statements_from_upserts();
-#EOF
+print_timestamp "Starting community statements..."
+psql $DB_URL <<EOF
+    INSERT INTO pgwar.initialization (msg, tmsp) VALUES ('start community statements', CLOCK_TIMESTAMP());
+    SELECT pgwar.update_community_statements_from_upserts();
+EOF
 
 # Start VACUUM ANALYZE
-#print_timestamp "Starting VACUUM ANALYZE..."
-#psql $DB_URL <<EOF
-#    INSERT INTO pgwar.initialization (msg, tmsp) VALUES ('VACUUM ANALYZE', CLOCK_TIMESTAMP());
-#    VACUUM ANALYZE;
-#EOF
+print_timestamp "Starting VACUUM ANALYZE..."
+psql $DB_URL <<EOF
+    INSERT INTO pgwar.initialization (msg, tmsp) VALUES ('VACUUM ANALYZE', CLOCK_TIMESTAMP());
+    VACUUM ANALYZE;
+EOF
 
 # Start full texts
-#print_timestamp "Starting full texts..."
-#psql $DB_URL <<EOF
-#    INSERT INTO pgwar.initialization (msg, tmsp) VALUES ('start fulltexts', CLOCK_TIMESTAMP());
-#    SELECT pgwar.update_full_texts(100000000);
-#EOF
+print_timestamp "Starting full texts..."
+psql $DB_URL <<EOF
+    INSERT INTO pgwar.initialization (msg, tmsp) VALUES ('start fulltexts', CLOCK_TIMESTAMP());
+    SELECT pgwar.update_full_texts(100000000);
+EOF
 
 # Start entity class metadata
 print_timestamp "Starting entity class metadata..."
